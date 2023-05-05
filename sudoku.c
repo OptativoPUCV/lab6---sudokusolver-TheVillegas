657f510806f1c9b3ec5460bae7c631446de7fd16
Replit user <>
2023-05-05 19:30:03 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -62,22 +62,27 @@ int is_valid(Node* n){
      {
       if (vfilas[n->sudo[i][j]] == 1)
       {
-        if(n->sudo[i][j] == 0)
+        if(n->sudo[i][j] != 0)
+        {
          return 0;
+        }
       }
       else{
         vfilas[n->sudo[i][j]] = 1;
       }
       if(vcolumnas[n->sudo[j][i]] == 1)
       {
-        if(n->sudo[j][i] == 0)
+        if(n->sudo[j][i] != 0)
+        {
          return 0;
+        }
       }
       else{
         vcolumnas[n->sudo[i][j]] = 1;
       }
      } 
   }
+  return 1;
   /*
   for(int k = 0 ; k < 9 ; k++) // recorre todos los elementos de la matriz
   {
@@ -99,7 +104,7 @@ int is_valid(Node* n){
 
   }
 */
- return 1;
+
 
 }
 
