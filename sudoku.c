0cdd294a43b91ff26139f296252d9c8c2a8f094d
Replit user <>
2023-05-05 19:31:05 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -63,23 +63,12 @@ int is_valid(Node* n){
       if (vfilas[n->sudo[i][j]] == 1)
       {
         if(n->sudo[i][j] != 0)
-        {
+          
          return 0;
-        }
       }
       else{
         vfilas[n->sudo[i][j]] = 1;
-      }
-      if(vcolumnas[n->sudo[j][i]] == 1)
-      {
-        if(n->sudo[j][i] != 0)
-        {
-         return 0;
-        }
-      }
-      else{
-        vcolumnas[n->sudo[i][j]] = 1;
-      }
+      }  
      } 
   }
   return 1;
@@ -104,7 +93,7 @@ int is_valid(Node* n){
 
   }
 */
-
+ return 1;
 
 }
 
